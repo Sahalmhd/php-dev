f424090622dd7657667c50db1e3f613300a0dbd0
sahal mhd <sahalmhd25@gmail.com>
2025-02-28 16:36:25 +0530

    views and partials

--- index.php
+++ index.php
@@ -3,41 +3,5 @@
 
 
 
-   $books = [
-
-      [
-         'name' => "life",
-         'auther' => "andywier",
-         'release_date'=> 2001,
-         'purchase_url' => "https//www.sahal.com"
-      ],
-      [
-         'name' => "life of me",
-         'auther' => "martin",
-         'release_date'=> 2002,
-         'purchase_url' => "https//www.sahal.com"
-      ],
-      [
-        'name' => "alogorthims",
-        'auther' => "martin",
-        'release_date'=> 2003,
-        'purchase_url' => "https//www.sahal.com"
-     ],
-   ];
-
-   $filter= function ($items,$fn ){
-
-      $filtedrdItems=[];
-      foreach($items as $item){
-         if($fn($item)){
-            $filtedrdItems[]=$item;
-         }
-      }
-      return $filtedrdItems;
-   };
-      $filterdBooks=array_filter($books, function($book){
-        return $book['release_date']>=2002;
-      });
    
-   
-      require 'index.view.php';
+      require 'views/index.view.php';
