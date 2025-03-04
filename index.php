bfb9f26b142efa1ac90255421f89c39a81e2f83a
sahal mhd <sahalmhd25@gmail.com>
2025-03-04 12:11:50 +0530

    new db connection

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 <?php
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+require 'Database.php';
+
+
+
+$db= new Database();
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+
+dd($posts);
